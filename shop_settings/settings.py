090fe25e8344b752230c479dde42d396f304bb79
m.malysh <m.malysh@mail.ocas.local>
2024-09-20 16:54:55 +0500

    split templates

--- shop_settings/settings.py
+++ shop_settings/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third party libraries
+    "sorl.thumbnail",
     "django_google_fonts",
     "mathfilters",
     "crispy_forms",
